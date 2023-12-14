54b3e67d66ed045e620c998889cd0d69271857cc
Alif Tasbir <88239141+a1if@users.noreply.github.com>
2023-12-14 15:03:38 +0600

    Update

---
+++ main.py
@@ -0,0 +1,5 @@
+from tomato import logger
+#from tomato.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+#from tomato.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+#from tomato.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+#from tomato.pipeline.stage_04_model_evaluation import EvaluationPipeline
