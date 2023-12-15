097bcfc0c46e84f299fe58d15b8324ad99693251
Alif Tasbir <88239141+a1if@users.noreply.github.com>
2023-12-15 16:47:35 +0600

    Pipeline added

--- main.py
+++ main.py
@@ -1,5 +1,57 @@
 from tomato import logger
-#from tomato.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-#from tomato.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-#from tomato.pipeline.stage_03_model_trainer import ModelTrainingPipeline
-#from tomato.pipeline.stage_04_model_evaluation import EvaluationPipeline
+from tomato.pipeline.stage_1 import DataIngestionTrainingPipeline
+from tomato.pipeline.stage_2 import PrepareBaseModelTrainingPipeline
+from tomato.pipeline.stage_3 import ModelTrainingPipeline
+from tomato.pipeline.stage_4 import EvaluationPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
